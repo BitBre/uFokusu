ed9d45167feb7d4e9ffc790b6bf79a350f248697
markonni93 <marko.djurdjev93@gmail.com>
2018-08-27 12:28:06 +0200

    Dodao kod za prikazivanje stranice ufokusu.rs unutar webview-a

--- app/src/main/java/rs/ufokusu/MainActivity.java
+++ app/src/main/java/rs/ufokusu/MainActivity.java
@@ -1,13 +1,34 @@
 package rs.ufokusu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView = (WebView) findViewById(R.id.webview);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        WebView myWebView = new WebView(getApplicationContext());
+        myWebView.loadUrl("https://www.ufokusu.rs");
+        myWebView.getSettings().setDomStorageEnabled(true);
+        setContentView(myWebView);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
     }
 }
+
+
